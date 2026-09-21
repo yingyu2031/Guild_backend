cebd541674aa377735d7c98ab7657a4a456d9766
yingyu2031 <30484861+yingyu2031@users.noreply.github.com>
2026-09-21 14:18:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
