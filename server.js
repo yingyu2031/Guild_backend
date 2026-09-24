68ace10ba2a6f0d29e9dc8bfd3b7c2440187d5fa
yingyu2031 <30484861+yingyu2031@users.noreply.github.com>
2026-09-24 14:26:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  // 可以在這裡確保連線使用台北時區
+  options: '-c timezone=Asia/Taipei'
+});
+
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
